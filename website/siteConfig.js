b1e995e9836fb9fa29012427c59a25e2189a34c3
Osio Alberto <osioalberto@gmail.com>
2019-05-16 11:37:28 +0200

    Added documentation about actions

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'React-RocketJump', // Title for your website.
+  tagline: 'RocketJump your React!',
+  url: 'https://inmagik.github.io', // Your website URL
+  baseUrl: '/react-rocketjump/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'react-rocketjump',
+  organizationName: 'InMagik srl',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -39,13 +39,13 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'installation', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'api_rj', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
-  users,
+  // users,
 
   /* path to images for header/footer */
   headerIcon: 'img/favicon.ico',
