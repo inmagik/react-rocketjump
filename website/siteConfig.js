97c363244c872b45b8d8ecee5d8dad4345b2fd51
gffuma <fumagalli.gf@gmail.com>
2019-05-21 00:26:21 +0200

    .gitignore in website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'react-rocketjump',
-  organizationName: 'InMagik srl',
+  organizationName: 'inmagik',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
