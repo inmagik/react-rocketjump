aa10dfd5842830135fd7f2ee9d831a3f3afec1f8
gffuma <fumagalli.gf@gmail.com>
2019-07-16 12:19:49 +0200

    Exports useRunRj from index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as useRj } from './useRj'
+export { default as useRunRj } from './useRunRj'
 export { default as connectRj } from './connectRj'
 export { default as rj } from './rj'
 export { default as compose } from './compose'
