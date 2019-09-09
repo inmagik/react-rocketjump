ffa7b8c1820ba0ff5483bde53431b23507e89e87
gffuma <fumagalli.gf@gmail.com>
2019-09-09 15:11:04 +0200

    Improve logger with component name

--- src/logger.js
+++ src/logger.js
@@ -5,6 +5,42 @@ import {
   RJ_TEARDOWN_EVENT,
 } from './debugger/index'
 
+function whereMyRjIsIvoked() {
+  const originalStackLimit = Error.stackTraceLimit
+  let stack
+  Error.stackTraceLimit = Infinity
+  try {
+    throw new Error()
+  } catch (e) {
+    stack = e.stack.split('\n')
+  }
+  Error.stackTraceLimit = originalStackLimit
+
+  const parseStackLine = line => {
+    const cleanLine = line.trim()
+    if (cleanLine.indexOf('at') === 0) {
+      const pieces = cleanLine.split(' ')
+      return pieces.slice(1)
+    }
+    return ['', '']
+  }
+  stack = stack.slice(1).map(parseStackLine)
+  let startedMainHook = false
+  for (let i = 0; i < stack.length; i++) {
+    const [functionName] = stack[i]
+    if (!startedMainHook && functionName === 'useMiniRedux') {
+      startedMainHook = true
+    } else if (startedMainHook) {
+      if (functionName.indexOf('use') !== 0) {
+        // First non hook from useMiniRedux
+        return `<${functionName} />`
+      }
+    }
+  }
+
+  return ''
+}
+
 function getRandomColor() {
   const letters = '0123456789ABCDEF'
   let color = '#'
@@ -20,21 +56,27 @@ const pad = n => Array.apply(null, { length: n }).join(' ')
 
 export default function rjLogger() {
   let rjLives = []
+  let whereUsed = {}
 
   RjDebugEvents.subscribe(event => {
     if (event.type === RJ_INIT_EVENT) {
+      whereUsed[event.meta.trackId] = whereMyRjIsIvoked()
       rjLives.push(event.meta.info)
     } else if (event.type === RJ_TEARDOWN_EVENT) {
       const index = rjLives.indexOf(event.meta.info)
       rjLives.splice(index, 1)
+      delete whereUsed[event.meta.trackId]
     } else if (event.type === RJ_DISPATCH_EVENT) {
       const { info, trackId } = event.meta
       const index = rjLives.indexOf(info)
       const color = colors[trackId % colors.length]
       const { action, prevState, nextState } = event.payload
       const rjName = info.name || `${index + 1}°`
+      const rjUsedFrom = whereUsed[trackId]
+
       console.groupCollapsed(
-        `%cRJ ${rjName} %caction %c${action.type}`,
+        `%c${rjUsedFrom}%c RJ ${rjName} %caction %c${action.type}`,
+        'color:#61dafb;background:#20232a;font-family:monospace;',
         `color:${color}`,
         'color:grey;font-weight:lighter;',
         'color:#464646;'
