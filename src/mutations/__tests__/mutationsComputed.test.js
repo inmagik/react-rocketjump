97a31a11be760c2152d537ffe78964f0cc4fd3c4
gffuma <fumagalli.gf@gmail.com>
2019-09-11 21:28:55 +0200

    Improve mutations state computed tests

--- src/mutations/__tests__/mutationsComputed.test.js
+++ src/mutations/__tests__/mutationsComputed.test.js
@@ -1,6 +1,7 @@
 import rj from '../../rj'
 import useRj from '../../useRj'
-import { renderHook } from '@testing-library/react-hooks'
+import { SUCCESS, FAILURE, PENDING } from '../../actionTypes'
+import { renderHook, act } from '@testing-library/react-hooks'
 
 describe('RJ mutations computed', () => {
   it('should work as expected without break ... for now', async () => {
@@ -34,17 +35,34 @@ describe('RJ mutations computed', () => {
   })
 
   it('should support @mutation computed', async () => {
+    const resolvesA = []
+    const effectKill = jest.fn(() => new Promise(r => resolvesA.push(r)))
+    const effectAlbi = jest.fn().mockResolvedValue(1312)
+
     const MaRjState = rj({
       mutations: {
         killHumans: {
-          effect: () => {},
-          updater: () => {},
-          reducer: () => ({ giova: 23 }),
+          effect: effectKill,
+          updater: s => s,
+          reducer: (state = { pending: false }, { type }) => {
+            if (type === PENDING) {
+              return { ...state, pending: true }
+            }
+            if (type === SUCCESS || type === FAILURE) {
+              return { ...state, pending: false }
+            }
+            return state
+          },
         },
         fuckBitches: {
-          effect: () => {},
-          updater: () => {},
-          reducer: () => ({ albi: 1312 }),
+          effect: effectAlbi,
+          updater: s => s,
+          reducer: (state = { albi: null }, action) => {
+            if (action.type === SUCCESS) {
+              return { albi: action.payload.data }
+            }
+            return state
+          },
         },
       },
       selectors: () => ({
@@ -56,7 +74,7 @@ describe('RJ mutations computed', () => {
       }),
       effect: () => Promise.resolve(1312),
       computed: {
-        ggg: '@mutation.killHumans',
+        kill: '@mutation.killHumans',
         albi: '@mutation.fuckBitches.albi',
         fumello: 'getData',
         magik: 'getMagik',
@@ -67,10 +85,41 @@ describe('RJ mutations computed', () => {
     expect(result.current[0]).toEqual({
       fumello: null,
       magik: 1312,
-      albi: 1312,
-      ggg: { giova: 23 },
+      albi: null,
+      kill: { pending: false },
     })
-    // TODO test this shit better
-    jest.tod('Test better with actions etc ..')
+
+    await act(async () => {
+      result.current[1].killHumans()
+    })
+
+    expect(result.current[0]).toEqual({
+      fumello: null,
+      magik: 1312,
+      albi: null,
+      kill: { pending: true },
+    })
+
+    await act(async () => {
+      resolvesA[0]()
+    })
+
+    expect(result.current[0]).toEqual({
+      fumello: null,
+      magik: 1312,
+      albi: null,
+      kill: { pending: false },
+    })
+
+    await act(async () => {
+      result.current[1].fuckBitches()
+    })
+
+    expect(result.current[0]).toEqual({
+      fumello: null,
+      magik: 1312,
+      albi: 1312,
+      kill: { pending: false },
+    })
   })
 })
