ff5b5204ccc1c94c9416f77fb886b630bef8ee33
gffuma <fumagalli.gf@gmail.com>
2020-09-30 22:29:14 +0200

    Experimental new plugin mutationsPending

--- example/pages/Todos/localstate.js
+++ example/pages/Todos/localstate.js
@@ -1,67 +1,51 @@
-import {
-  rj,
-  matchMutationType,
-  PENDING,
-  SUCCESS,
-  FAILURE,
-} from 'react-rocketjump'
+import { rj } from 'react-rocketjump'
 import rjPlainList from 'react-rocketjump/plugins/plainList'
+import rjMutationsPending from 'react-rocketjump/plugins/mutationsPending'
 import request from 'superagent'
 
 export const API_URL = 'http://localhost:9001'
 
-const trackTypes = ['removeTodo', 'toggleTodo']
-function mutationsBusyReducer(state = 0, action) {
-  if (matchMutationType(action.type, trackTypes, PENDING)) {
-    return state + 1
-  }
-  if (matchMutationType(action.type, trackTypes, [SUCCESS, FAILURE])) {
-    return state - 1
-  }
-  return state
-}
-
-export const TodosListState = rj(rjPlainList(), {
-  effect: () => request.get(`${API_URL}/todos`).then(({ body }) => body),
-  mutations: {
-    addStupidTodo: rj.mutation.single({
-      effect: (todo) =>
-        request
-          .post(`${API_URL}/todos`)
-          .send(todo)
-          .then(({ body }) => body),
-      updater: 'insertItem',
-    }),
-    removeTodo: rj.mutation.multi((todo) => todo.id, {
-      effect: (todo) =>
-        request.delete(`${API_URL}/todos/${todo.id}`).then(() => todo),
-      updater: 'deleteItem',
-    }),
-    toggleTodo: rj.mutation.multi((todo) => todo.id, {
-      effect: (todo) =>
-        request
-          .put(`${API_URL}/todos/${todo.id}`)
-          .send({
-            ...todo,
-            done: !todo.done,
-          })
-          .then(({ body }) => body),
-      updater: 'updateItem',
-    }),
-  },
-  selectors: () => ({
-    isBusy: (state) => state.busy > 0,
+export const TodosListState = rj(
+  rjPlainList(),
+  rjMutationsPending({
+    track: ['removeTodo', 'toggleTodo'],
   }),
-  combineReducers: {
-    busy: mutationsBusyReducer,
-  },
-  computed: {
-    busy: 'isBusy',
-    adding: '@mutation.addStupidTodo.pending',
-    deleting: '@mutation.removeTodo.pendings',
-    updating: '@mutation.toggleTodo.pendings',
-    todos: 'getData',
-    loading: 'isPending',
-  },
-  name: 'MaTodos',
-})
+  {
+    effect: () => request.get(`${API_URL}/todos`).then(({ body }) => body),
+    mutations: {
+      addStupidTodo: rj.mutation.single({
+        effect: (todo) =>
+          request
+            .post(`${API_URL}/todos`)
+            .send(todo)
+            .then(({ body }) => body),
+        updater: 'insertItem',
+      }),
+      removeTodo: rj.mutation.multi((todo) => todo.id, {
+        effect: (todo) =>
+          request.delete(`${API_URL}/todos/${todo.id}`).then(() => todo),
+        updater: 'deleteItem',
+      }),
+      toggleTodo: rj.mutation.multi((todo) => todo.id, {
+        effect: (todo) =>
+          request
+            .put(`${API_URL}/todos/${todo.id}`)
+            .send({
+              ...todo,
+              done: !todo.done,
+            })
+            .then(({ body }) => body),
+        updater: 'updateItem',
+      }),
+    },
+    computed: {
+      busy: 'anyMutationsPending',
+      adding: '@mutation.addStupidTodo.pending',
+      deleting: '@mutation.removeTodo.pendings',
+      updating: '@mutation.toggleTodo.pendings',
+      todos: 'getData',
+      loading: 'isPending',
+    },
+    name: 'MaTodos',
+  }
+)
