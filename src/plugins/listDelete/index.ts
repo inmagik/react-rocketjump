5cf0e0bd82921591c34c379deb776aa4bdcb1c3f
gffuma <fumagalli.gf@gmail.com>
2021-02-15 14:09:08 +0100

    Improve plugins list and related. Plugins listInsert and listDelete avoid shipping pagination warning in production builds. Plugin list set paginationWarning false to insert and delete list plugins implement the count reconciliation but provide two additional options in list plugin to supress them.

--- src/plugins/listDelete/index.ts
+++ src/plugins/listDelete/index.ts
@@ -3,7 +3,7 @@ import { get } from '../../core/utils'
 import { set } from 'object-path-immutable'
 import { Action } from '../../core/types'
 
-const TYPE = 'RJ_LIST_DELETE'
+export const TYPE = 'RJ_LIST_DELETE'
 
 const defaultIdentity = (action: Action, listItem: any) =>
   action.item.id === listItem.id
@@ -24,21 +24,24 @@ const rjListDelete = (config: RjListDeletePluginConfig = {}) => {
     }),
     reducer: (oldReducer) => (state, action) => {
       if (action.type === TYPE) {
-        const paginationPath = path
-          .split('.')
-          .map((item, i, arr) => (i === arr.length - 1 ? 'pagination' : item))
-          .join('.')
-        if (
-          state &&
-          get(state, paginationPath) &&
-          config.warnPagination !== false
-        ) {
-          console.warn(
-            'It seems you are using this plugin on a paginated list. Remember that this plugin is agnostic wrt pagination, and will break it. To suppress this warning, set warnPagination: false in the config object'
-          )
+        if (process.env.NODE_ENV !== 'production') {
+          if (config.warnPagination !== false) {
+            const paginationPath = path
+              .split('.')
+              .map((item, i, arr) =>
+                i === arr.length - 1 ? 'pagination' : item
+              )
+              .join('.')
+            if (state && get(state, paginationPath)) {
+              console.warn(
+                'It seems you are using this plugin on a paginated list. Remember that this plugin is agnostic wrt pagination, and will break it. To suppress this warning, set warnPagination: false in the config object'
+              )
+            }
+          }
         }
         let list = get(state, path)
-        if (list) list = list.filter((listItem: any) => !identity(action, listItem))
+        if (list)
+          list = list.filter((listItem: any) => !identity(action, listItem))
         return set(state, path, list)
       }
       return oldReducer(state, action)
--- src/plugins/listInsert/index.ts
+++ src/plugins/listInsert/index.ts
@@ -3,7 +3,7 @@ import { get } from '../../core/utils'
 import { set } from 'object-path-immutable'
 import { Action } from '../../core/types'
 
-const TYPE = 'RJ_LIST_INSERT'
+export const TYPE = 'RJ_LIST_INSERT'
 
 interface RjListInsertPluginConfig {
   path?: string
@@ -23,18 +23,20 @@ const rjListInsert = (config: RjListInsertPluginConfig = {}) => {
     }),
     reducer: (oldReducer) => (state, action) => {
       if (action.type === TYPE) {
-        const paginationPath = path
-          .split('.')
-          .map((item, i, arr) => (i === arr.length - 1 ? 'pagination' : item))
-          .join('.')
-        if (
-          state &&
-          get(state, paginationPath) &&
-          config.warnPagination !== false
-        ) {
-          console.warn(
-            'It seems you are using this plugin on a paginated list. Remember that this plugin is agnostic wrt pagination, and will break it. To suppress this warning, set warnPagination: false in the config object'
-          )
+        if (process.env.NODE_ENV !== 'production') {
+          if (config.warnPagination !== false) {
+            const paginationPath = path
+              .split('.')
+              .map((item, i, arr) =>
+                i === arr.length - 1 ? 'pagination' : item
+              )
+              .join('.')
+            if (state && get(state, paginationPath)) {
+              console.warn(
+                'It seems you are using this plugin on a paginated list. Remember that this plugin is agnostic wrt pagination, and will break it. To suppress this warning, set warnPagination: false in the config object'
+              )
+            }
+          }
         }
         const mergeFunc = config.merge ? config.merge : defaultMerge
         return set(state, path, mergeFunc(action, get(state, path)))
